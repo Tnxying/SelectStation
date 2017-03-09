3617eeef8d4057841136943216b543ab5d553981
Tnxying <tieyingfeag@163.com>
2017-03-09 14:18:22 +0800

    Signed-off-by: Tnxying <tieyingfeag@163.com>

--- SelectStation.py
+++ SelectStation.py
@@ -3,12 +3,18 @@ from urllib import request
 
 # url = 'http://221.226.28.67:88/jsswxxSSI/water_selectWaterAndStStbprpBAndWaterWarJson.action'
 url = "http://www.baidu.com"
-try:
-    response = request.urlopen(url)
-    html = bytes.decode(response.read())
-    print(response)
-    print(html)
-    f=open('test.html', 'w', -1,'utf-8')
-    f.write(html)
-except (Exception, request.URLError) as e:
-    print(e)
+
+
+def getUrlData(url):
+    try:
+        response = request.urlopen(url)
+        html = bytes.decode(response.read())
+        print(response)
+        print(html)
+        f = open('test.html', 'w', -1, 'utf-8')
+        f.write(html)
+    except (Exception, request.URLError) as e:
+        print(e)
+
+if __name__ == "__main__":
+    getUrlData()
